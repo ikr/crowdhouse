0117914304637e5b39d6495c90fe495e57b6f977
Ivan Krechetov <ikr@ikr.su>
2018-07-13 13:16:16 +0200

    Add 404-s

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 
 const server = require('server')
 const PgPool = require('pg').Pool
+const redis = require('redis')
 const {VError} = require('verror')
 const newServer = require('./src/newServer')
 
@@ -10,9 +11,11 @@ if (process.argv.length < 3) {
 }
 
 const pgPool = new PgPool({connectionString: 'postgres://postgres@127.0.0.1/salesforce'})
+const pgQuery = pgPool.query.bind(pgPool)
+const rClient = redis.createClient()
 
-server({port: parseInt(process.argv[2])}, newServer({pgPool})).then(() => {
-    console.log(`ota-access listening on port ${process.argv[2]}`)
+server({port: parseInt(process.argv[2])}, newServer({pgQuery, rClient})).then(() => {
+    console.log(`crowdhouse listening on port ${process.argv[2]}`)
 }, error => {
     console.dir(new VError(error, 'Failed to start up the HTTP server'))
     process.exit(1)
--- src/newServer.js
+++ src/newServer.js
@@ -1,8 +1,12 @@
 const {join} = require('path')
-const {get} = require('server/router')
-const {type} = require('server/reply')
+const {get, put, error} = require('server/router')
+const {type, status, send} = require('server/reply')
 
-module.exports = function () {
+function notFound () {
+    return status(404).send('No matching route found')
+}
+
+module.exports = function ({pgQuery, rClient}) {
     return [
         get(
             '/doc',
@@ -10,6 +14,18 @@ module.exports = function () {
                 join(__dirname, '../README.md'),
                 'README.md'
             )
-        )
+        ),
+
+        put(
+            '/users/:id',
+            ctx => {
+                console.log(111111111111)
+                return send('ssss')
+            }
+        ),
+
+        get(notFound),
+        put(notFound),
+        error(ctx => status(500).type('text/plain').send(ctx.error.trace))
     ]
 }
